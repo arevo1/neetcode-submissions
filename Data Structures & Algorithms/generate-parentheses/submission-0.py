e9a02ede416244e31be75388685abc49d99f97f2
aeg0n <a.revo360@gmail.com>
2026-07-19 20:40:33 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+
+        def backtrack(current: str, opened: int, closed: int) -> None:
+            if len(current) == 2 * n:
+                result.append(current.copy())
+                return
+
+            if opened < n:
+                backtrack(current + "(", opened + 1, closed)
+
+            if closed < opened:
+                backtrack(current + ")", opened, closed + 1)
+
+        backtrack("", 0, 0)
+        return result
