4f579be0795148c55ef1a50989d3af9dae58c2f4
aeg0n <a.revo360@gmail.com>
2026-07-20 20:47:22 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one_step_before = 1
+        two_steps_before = 1
+
+        for _ in range(n):
+            current = one_step_before + two_steps_before
+            two_steps_before = one_step_before
+            one_step_before = current
+
+        return two_steps_before
